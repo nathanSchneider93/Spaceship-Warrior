2882fc1d11873bf3052d9b639779c2d3f3ea7fe2
Lucas Franco <lucaslemosfranco@hotmail.com>
2020-05-08 22:15:22 -0300

    Start implementation of PlayerCannon and its related classes

---
+++ SpaceshipWarrior_Project/Assets/_Project/Scripts/Player/PlayerCannonProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SpaceshipWarrior.PlayerModule
+{
+    public class PlayerCannonProjectile : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private int _damage;
+        [SerializeField] private float _speed;
+
+        public void Initialize(Vector3 direction)
+        {
+            _rigidbody.velocity = direction * _speed;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var damageable = other.gameObject.GetComponent<IDamageable>();
+            damageable?.TakeDamage(_damage);
+        }
+    }
+}
