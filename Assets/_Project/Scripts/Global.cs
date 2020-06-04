4c1ddd88a6b871d379fcdb4dba7679cdc132143a
Lucas Franco <lucaslemosfranco@hotmail.com>
2020-06-03 21:48:42 -0300

    Rename script related folders and remove old scripts

--- Assets/_Project/Scripts/Global.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assets._Scripts
-{
-    public static class Global
-    {
-        public static int nivelDanos = 0;
-        public static int placarJogo = 0;
-        public static int navesDestruidas = 0;
-    }
-}
